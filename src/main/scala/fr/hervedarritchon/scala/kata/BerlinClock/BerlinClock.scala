ec278fa5380d6f1617aed541f8a8a54e4aa5df38
Hervé Darritchon <herve.darritchon@gmail.com>
2015-05-23 00:19:08 +0200

    Evolution: Creation of the BerlinClock plumbing. Example of tests, main class and BerlinClock class.

---
+++ src/main/scala/fr/hervedarritchon/scala/kata/BerlinClock/BerlinClock.scala
@@ -0,0 +1,11 @@
+package fr.hervedarritchon.scala.kata.berlinclock
+
+/**
+ * Created by Hervé Darritchon on 23/05/15.
+ *
+ */
+object BerlinClock {
+
+  def start() = ???
+
+}
---
+++ src/test/scala/fr/hervedarritchon/scala/kata/berlinclock/BerlinClockSpec.scala
@@ -0,0 +1,9 @@
+package fr.hervedarritchon.scala.kata.berlinclock
+
+/**
+ * Created by Hervé Darritchon on 23/05/15.
+ *
+ */
+class BerlinClockSpec {
+
+}
